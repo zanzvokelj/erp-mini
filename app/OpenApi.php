fc7af332e11f2b94a4d3f2406d5870def53e6a64
ZanZvokelj <zan.zvokelj@academiavss.si>
2026-03-08 18:02:36 +0100

    updating apis

---
+++ app/OpenApi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="Mini ERP API",
+ *     version="1.0.0",
+ *     description="REST API for the Mini ERP Inventory and Order Management System"
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000/api/v1",
+ *     description="Local API Server"
+ * )
+ */
+class OpenApi
+{
+}
